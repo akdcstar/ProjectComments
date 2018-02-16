4dc859b334ff8ddf9131fbe30d488138e4ea25a3
akdcstar <akdcstar@gmail.com>
2018-02-16 12:38:46 +0200

    Add tests for classes `LimitedRoom` and `UnlimitedRoom`
    #8

--- src/test/java/net/comments/sample/UnlimitedRoomTest.java
+++ src/test/java/net/comments/sample/UnlimitedRoomTest.java
@@ -1,6 +1,23 @@
 package net.comments.sample;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.collection.IsIterableContainingInOrder;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UnlimitedRoomTest {
 
+    @Test
+    public void testGetPersonInRoom() {
+        List<Person> personList = new ArrayList<Person>();
+        Person andrey = new AttentiveStudent("andrey");
+        Person sasha = new TechTeacher("sasha");
+        personList.add(andrey);
+        personList.add(sasha);
 
+        MatcherAssert.assertThat(personList, IsIterableContainingInOrder.contains(andrey, sasha));
+//        MatcherAssert.assertThat(personList, IsIterableContainingInAnyOrder.containsInAnyOrder(sasha, andrey));
+    }
 }
