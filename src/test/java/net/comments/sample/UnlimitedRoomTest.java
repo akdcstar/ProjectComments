b4519646df5c8cd65846c8557b221b7cf878ab45
akdcstar <akdcstar@gmail.com>
2018-02-19 17:53:16 +0200

    Add new unit test for `LimitedRoom` and `UnlimitedRoom`
    #8

--- src/test/java/net/comments/sample/UnlimitedRoomTest.java
+++ src/test/java/net/comments/sample/UnlimitedRoomTest.java
@@ -4,20 +4,13 @@ import org.hamcrest.MatcherAssert;
 import org.hamcrest.collection.IsIterableContainingInOrder;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class UnlimitedRoomTest {
 
     @Test
     public void testGetPersonInRoom() {
-        List<Person> personList = new ArrayList<Person>();
+        UnlimitedRoom roomOne = new UnlimitedRoom();
         Person andrey = new AttentiveStudent("andrey");
-        Person sasha = new TechTeacher("sasha");
-        personList.add(andrey);
-        personList.add(sasha);
-
-        MatcherAssert.assertThat(personList, IsIterableContainingInOrder.contains(andrey, sasha));
-//        MatcherAssert.assertThat(personList, IsIterableContainingInAnyOrder.containsInAnyOrder(sasha, andrey));
+        roomOne.accept(andrey);
+        MatcherAssert.assertThat(roomOne.getPersonInRoom(), IsIterableContainingInOrder.contains(andrey));
     }
 }
